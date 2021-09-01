8e57aa04218427db64239ea813f36f86a08501a4
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-09-01 15:25:05 -0700

    finished project 2

--- forms_starter/src/components/form/Form.js
+++ forms_starter/src/components/form/Form.js
@@ -12,34 +12,41 @@ const Form = () => {
 
     useEffect(() => {
         const errors = []
+        let regexPhone = /\d\d\d-\d\d\d-\d\d\d\d/
+        let regexEmail = /.+@.+\.(com)/
         if (!name) errors.push('need to type a name');
         if (!email) errors.push('need to type a email');
         if (!phonenumber) errors.push('need to type a phonenumber');
         if (!usertype) errors.push('need to select a usertype');
         if (!bio) errors.push('need to type a bio');
+        if (bio.length > 280) errors.push("max bio char limit of 280")
+        if (!email.match(regexEmail)) errors.push("incorrect email format")
+        if(!phonenumber.match(regexPhone)) errors.push("incorrect phone format")
         setValidationErrors(errors)
     }, [name, email, phonenumber, usertype, bio, signup])
 
+    console.log(phonenumber.length)
     let onSubmit = (e) => {
         e.preventDefault()
 
     }
-
     return(
         <div>
-            {validationErrors.length &&
-            validationErrors.map(err => err)}
-
-            <form className='' onSubmit={onSubmit}>
-                <input type="text" placeholder='Name' onChange={(e) => setName(e.target.value)} >Name</input>
-                <input type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)}>Email</input>
-                <input type="text" placeholder='Phone number' onChange={(e) => setPhonenumber(e.target.value)}>Phone number</input>
-                <input type='radio' id='instructor' name='instructor' onChange={(e) => setUsertype('Instructor')}></input>
-                    <label HTMLfor='instructor'>Instructor</label>
-                <input type='radio' id='Student' name='Student' onChange={(e) => setUsertype('Student')}></input>
-                    <label HTMLfor='Student'>Student</label>
-                <textarea onChange={(e) => setBio(e.target.value)}>Bio</textarea>
-                <input type="checkbox" onChange={(e) => setSignup(e.target.checked)}>Sign up for email notifications</input>
+            {validationErrors &&
+            validationErrors.map(err => {
+                return <div>{err}</div>
+                })}
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder='Name' onChange={(e) => setName(e.target.value)}/>
+                <input type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
+                <input type="text" placeholder='Phone number' onChange={(e) => setPhonenumber(e.target.value)}/>
+                <input type='radio' id='instructor' name='instructor' onChange={(e) => setUsertype('Instructor')}/>
+                <label htmlFor='instructor'>Instructor</label>
+                <input type='radio' id='Student' name='Student' onChange={(e) => setUsertype('Student')}/>
+                <label htmlFor='Student'>Student</label>
+                <textarea onChange={(e) => setBio(e.target.value)} placeholder = "Bio"></textarea>
+                <input type="checkbox" onChange={(e) => setSignup(e.target.checked)}/>Sign up for email notifications
+                <input type = "submit" value = "submit"></input>
             </form>
         </div>
     )
